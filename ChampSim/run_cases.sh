3aa214486045e26e121393f461ea1a82e1ee4785
sudoRicheek <richeekdas2001@gmail.com>
2021-11-27 14:18:26 +0530

    Updated run_cases.sh

--- ChampSim/run_cases.sh
+++ ChampSim/run_cases.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-./build_champsim.sh ltagenew no no no no lru 1
-./build_champsim.sh tagenew no no no no lru 1
+./build_champsim.sh ltage no no no no lru 1
+./build_champsim.sh tage no no no no lru 1
 ./build_champsim.sh hashed_perceptron no no no no lru 1
 
 
-./run_champsim.sh ltagenew-no-no-no-no-lru-1core 10 10 server_001.champsimtrace.xz
-./run_champsim.sh ltagenew-no-no-no-no-lru-1core 10 10 server_002.champsimtrace.xz
-./run_champsim.sh ltagenew-no-no-no-no-lru-1core 10 10 server_003.champsimtrace.xz
-./run_champsim.sh ltagenew-no-no-no-no-lru-1core 10 10 server_004.champsimtrace.xz
-./run_champsim.sh ltagenew-no-no-no-no-lru-1core 10 10 server_009.champsimtrace.xz
+./run_champsim.sh ltage-no-no-no-no-lru-1core 10 10 server_001.champsimtrace.xz
+./run_champsim.sh ltage-no-no-no-no-lru-1core 10 10 server_002.champsimtrace.xz
+./run_champsim.sh ltage-no-no-no-no-lru-1core 10 10 server_003.champsimtrace.xz
+./run_champsim.sh ltage-no-no-no-no-lru-1core 10 10 server_004.champsimtrace.xz
+./run_champsim.sh ltage-no-no-no-no-lru-1core 10 10 server_009.champsimtrace.xz
 
-./run_champsim.sh tagenew-no-no-no-no-lru-1core 10 10 server_001.champsimtrace.xz
-./run_champsim.sh tagenew-no-no-no-no-lru-1core 10 10 server_002.champsimtrace.xz
-./run_champsim.sh tagenew-no-no-no-no-lru-1core 10 10 server_003.champsimtrace.xz
-./run_champsim.sh tagenew-no-no-no-no-lru-1core 10 10 server_004.champsimtrace.xz
-./run_champsim.sh tagenew-no-no-no-no-lru-1core 10 10 server_009.champsimtrace.xz
+./run_champsim.sh tage-no-no-no-no-lru-1core 10 10 server_001.champsimtrace.xz
+./run_champsim.sh tage-no-no-no-no-lru-1core 10 10 server_002.champsimtrace.xz
+./run_champsim.sh tage-no-no-no-no-lru-1core 10 10 server_003.champsimtrace.xz
+./run_champsim.sh tage-no-no-no-no-lru-1core 10 10 server_004.champsimtrace.xz
+./run_champsim.sh tage-no-no-no-no-lru-1core 10 10 server_009.champsimtrace.xz
 
 ./run_champsim.sh hashed_perceptron-no-no-no-no-lru-1core 10 10 server_001.champsimtrace.xz
 ./run_champsim.sh hashed_perceptron-no-no-no-no-lru-1core 10 10 server_002.champsimtrace.xz
